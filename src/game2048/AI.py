21ec8703de75ba9474eaac6ed56dd5c2445a87f4
Eric Bui <buitrung.phuongthao@gmail.com>
2026-08-01 23:22:00 +0300

    Add comment to class AI

--- src/game2048/AI.py
+++ src/game2048/AI.py
@@ -1,19 +1,45 @@
-from node import Node
-from game import Game
+from game2048.node import Node
+from game2048.game import Game
+from game2048.board import Board
 import copy
+import random
 class AI():
-    def new_game(self):
-        print("Start game")
-        self._game = Game()
-        self.play(4)
+    """A class that maintains the functionality of the AI in a game
 
-    def play(self,max_depth):
+    Attributes:
+        game: the game that the AI is playing
+    """
+    def new_game(self, board = None, random_generator = random.Random()):
+        """Start a new game, a new Game object is created
+
+        Args:
+            board: the current game board, board is empty if no given argue
+            random_generator: the class containing methods to generate random values. 
+            Normally random.Random()
+        """
+        print("Start game")
+        if board == None:
+            self._game = Game(random_generator=random_generator)
+            self._game.init_board()
+        else:
+            self._game = Game(board, random_generator)
+
+    @property
+    def game(self):
+        return self._game
+    
+    def play(self,max_depth,continuous = True):
+        """AI starts playing when play is called
+
+        Args:
+            max_depth: maximum depth of the search tree
+            continuous: whether the AI continues to play the game on its own,
+            default value is True
+        """
         while True:
             if self._game.game_over():
                 break
-            copy_board = copy.deepcopy(self._game.board())
-            self._game_tree = Node(is_max = True, board = copy_board)
-            move = self.expectimax(self._game_tree,0,max_depth,True)[0]
+            move = self.best_move(max_depth)
             if move == "left":
                 self._game.move_left()
             elif move == "right":
@@ -22,18 +48,43 @@ class AI():
                 self._game.move_up()
             elif move == "down":
                 self._game.move_down()
+            if not continuous:
+                break
 
+    def best_move(self, max_depth, directions = ("left","right","up","down"), game = None):
+        """Finds the best move from the give list of move
+
+        Args:
+            max_depth: maximum depth of the search tree
+            directions: possible moves to evaluate
+            game: the Game object to play, use the class attribute
+            if no game is given
+
+        Returns: the best move found from executing expectimax
+        """
+        copy_board = copy.deepcopy(self._game.board) if game == None else copy.deepcopy(game.board)
+        self._game_tree = Node(is_max = True, board = copy_board)
+        return self.expectimax(self._game_tree, 0, max_depth, directions)
     
-    def expectimax(self, root_node: Node, depth,max_depth, is_max):
+    def expectimax(self, root_node: Node, depth,max_depth, directions = ("left","right","up","down")):
+        """Search the game tree to find the best move
+
+        Args:
+            root_node (Node): the root node containing the current board state
+            depth: search depth, increase by 1 after each search
+            max_depth: maximum search depth 
+            directions: moves to find best move from
+        Returns: the best move (left/right/up/down)
+        """
         if depth == max_depth:
-            return root_node.board().heuristic()
-        board = root_node.board()
-        if is_max:
-            for d in ("left","right","up","down"):
+            return root_node.board.heuristic()
+        board = root_node.board
+        if root_node.is_max:
+            for d in directions:
                 copy_board = copy.deepcopy(board)
                 if d == "left":
                     copy_board.move_left()
-                    if copy_board.board() == board.board():
+                    if copy_board.board == board.board:
                         continue
                     child_node = Node(False,copy_board)
                     if depth == 0:
@@ -41,10 +92,10 @@ class AI():
                     else:
                         child = child_node
                     root_node.add_child(child)
-                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth,False))
+                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth))
                 if d == "right":
                     copy_board.move_right()
-                    if copy_board.board() == board.board():
+                    if copy_board.board == board.board:
                         continue
                     child_node = Node(False,copy_board)
                     if depth == 0:
@@ -52,10 +103,10 @@ class AI():
                     else:
                         child = child_node
                     root_node.add_child(child)
-                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth,False))
+                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth))
                 if d == "up":
                     copy_board.move_up()
-                    if copy_board.board() == board.board():
+                    if copy_board.board == board.board:
                         continue
                     child_node = Node(False,copy_board)
                     if depth == 0:
@@ -63,10 +114,10 @@ class AI():
                     else:
                         child = child_node
                     root_node.add_child(child)
-                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth,False))
+                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth))
                 if d == "down":
                     copy_board.move_down()
-                    if copy_board.board() == board.board():
+                    if copy_board.board == board.board:
                         continue    
                     child_node = Node(False,copy_board)
                     if depth == 0:
@@ -74,31 +125,31 @@ class AI():
                     else:
                         child = child_node
                     root_node.add_child(child)
-                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth,False))
+                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth))
+            if root_node.children == []:
+                return board.heuristic() if depth != 0 else None
             value = 0
             move = "left"
-            for child in root_node.children():
+            for child in root_node.children:
                 if depth == 0:
-                    if child[1].value() > value:
+                    if child[1].value > value:
                         move = child[0]
-                        value = child[1].value()
-                elif depth > 0:
-                    value = max(value,child.value())
-            return value if depth != 0 else (move,value)
+                        value = child[1].value
+                else:
+                    value = max(value,child.value)
+            return value if depth != 0 else move
         else:
-            for i in range(4):
-                for j in range(4):
-                    if board.get_value((i,j)) != 0:
-                        continue
-                    for val in [(0.9,2),(0.1,4)]:
-                        copy_board = copy.deepcopy(board)
-                        copy_board.set_value((i,j),val[1])
-                        child_node = Node(True,copy_board)
-                        child = (val[0],child_node)
-                        root_node.add_child(child)
-                        child_node.set_value(self.expectimax(child_node,depth+1,max_depth,True))
+            empty_tiles = board.find_empty_tiles()
+            for i,j in empty_tiles:
+                for val in [(0.9,2),(0.1,4)]:
+                    copy_board = copy.deepcopy(board)
+                    copy_board.set_value((i,j),val[1])
+                    child_node = Node(True,copy_board)
+                    child = (val[0],child_node)
+                    root_node.add_child(child)
+                    child_node.set_value(self.expectimax(child_node,depth+1,max_depth))
             value = 0
-            for child in root_node.children():
-                value += child[0]*child[1].value()
-            value /= (len(root_node.children())/2)
+            for child in root_node.children:
+                value += child[0]*child[1].value
+            value /= (len(root_node.children)/2)
             return value
